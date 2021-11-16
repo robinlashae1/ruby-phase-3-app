73d2c18c64bc7e69dedab51156a36c3a2aa4a7d0
Saim Rizvi <shr2mz@gmail.com>
2021-11-16 09:59:44 -0500

    addcommunication modal

--- src/Components/JobContainer.js
+++ src/Components/JobContainer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import JobCard from './JobCard';
 
- function JobContainer ({jobData}){
-    console.log(jobData)
+ function JobContainer ({jobData, handleModal}){
+    
     return(
         <div>
             <h1>Jobs</h1>
-            <div>{jobData.map(job => <JobCard key={job.id} job={job} />)}</div>
+            <div>{jobData.map(job => <JobCard key={job.id} job={job} handleModal={handleModal}/>)}</div>
         </div>
     )
 }
