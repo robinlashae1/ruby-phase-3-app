f75f9d8a76ed31dd2ed81c7f687b30eacaf32a5d
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-11-17 11:11:08 -0500

    Update status dropdowns

--- src/Components/DisplayPanel.js
+++ src/Components/DisplayPanel.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import JobCard from './JobCard';
- function DisplayPanel ({title, jobData, status, communicationData}){
+ function DisplayPanel ({title, jobData, status, communicationData, updateJob}){
 const filteredJobs= jobData.filter((job)=>(
     job.status === status
 ))
 const jobCard = filteredJobs.map((job)=>(
-    <JobCard key={job.id} job={job} communicationData={communicationData.filter((communication) => {return communication.application_id === job.id} )} />
+    <JobCard key={job.id} job={job} updateJob={updateJob} communicationData={communicationData.filter((communication) => {return communication.application_id === job.id} )} />
 ))
     return(
         <div className= "displayPanels">
--- src/Components/JobCard.js
+++ src/Components/JobCard.js
@@ -1,12 +1,34 @@
 
 import {useEffect, useState} from 'react';
 
-function JobCard({job, communicationData}) {
+function JobCard({job, communicationData, updateJob}) {
     const [value, setValue] = useState("default")
+    const [dropdownDisabled, setDropdownDisabled] = useState(false);
 
     const handleChange = (e) => {
-        setValue(e.target.value);
-      };
+        const options = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                user_id: localStorage.getItem('user_id'),
+                login_token: localStorage.getItem('login_token'),
+                status: e.target.value
+            })
+        };
+        setDropdownDisabled(true);
+        fetch(`http://localhost:9292/applications/${job.id}`, options)
+            .then(resp => resp.json())
+            .then(data => {
+                if (data.success) {
+                    updateJob(data.data);
+                } else {
+                    console.log(data);
+                }
+                setDropdownDisabled(false);
+            });
+    };
   
       const [displayCommunication, setDisplayCommunication] = useState(false)
 
@@ -19,12 +41,13 @@ function JobCard({job, communicationData}) {
             <h3 className="jobText">{job.company}</h3>
 
             <h4>{job.position}</h4>
-                 <select onChange={handleChange} id="select-button">
-                <option value="default" disabled hidden>Update?</option>
-                 <option value="1" className="dropdownitem">Wishlist</option>
-                 <option value="2" className="dropdownitem">Applied</option>
-                <option value="3" className="dropdownitem">Interview</option>
-                <option value="4" className="dropdownitem">Rejected</option>
+                 <select onChange={handleChange} id="select-button" value={job.status} disabled={dropdownDisabled}>
+                {/* <option value="default" disabled hidden>Update?</option> */}
+                 <option value="wishlist" className="dropdownitem">Wishlist</option>
+                 <option value="pending" className="dropdownitem">Applied</option>
+                <option value="interviewing" className="dropdownitem">Interviewing</option>
+                <option value="offer made" className="dropdownitem">Offer Made</option>
+                <option value="rejected" className="dropdownitem">Rejected</option>
                  </select>
                                 {displayCommunication ? (
             <div>
